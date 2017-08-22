78dad7f1e64ccabdd2964ab8949d7775dbaff5a4
olliechick <och26@uclive.ac.nz>
2017-08-22 14:46:26 +1200

    Start CSV parsing using a CSV parser library (and added dependency to POM file). Added target/ to .gitignore.

--- src/main/java/seng202/team1/CSVLoader.java
+++ src/main/java/seng202/team1/CSVLoader.java
@@ -1,9 +1,50 @@
 package seng202.team1;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
 /**
  * Class that will pull in our CSV files and then call the appropriate constructor.
  * Most likely will be static I'm thinking. Should allow us to use it without creating an instance.
+ * Last updated 22 August 2017.
  * @author Josh Burt
+ * @author Ollie Chick
  */
 public class CSVLoader {
+
+    /*
+    * Takes a file named filename, which should be a csv, and processes it.
+    *
+    * @author Ollie Chick
+    * @param filename The filename of the file to get data from.
+    * @throws FileNotFoundException If the file can't be found.
+     */
+    public static void loadCSV(String filename) throws IOException {
+        File csvData = new File(filename);
+        CSVParser parser = CSVParser.parse(csvData, Charset.defaultCharset(), CSVFormat.RFC4180);
+        for (CSVRecord csvRecord : parser) {
+            System.out.println(csvRecord.get(0));
+        }
+
+    }
+
+
+    /*
+    * Main class
+    * Just used for testing
+    * @author Ollie Chick
+     */
+    public static void main(String[] args) {
+        String filename = "bikedata.csv";
+        try {
+            loadCSV(filename);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
 }
