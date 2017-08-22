df7d3a117021507874f927455c857a6a94246c26
olliechick <och26@uclive.ac.nz>
2017-08-22 12:17:45 +1200

    Create skeleton for FileProcessor class

---
+++ src/main/java/seng202/team1/FileProcessor.java
@@ -0,0 +1,17 @@
+package seng202.team1;
+
+/**
+ * Created by och26 on 22/08/17.
+ */
+public class FileProcessor {
+
+    public loadCSV(String filename) {
+
+    }
+
+    public saveCSV(String filename) {
+
+
+    }
+
+}
