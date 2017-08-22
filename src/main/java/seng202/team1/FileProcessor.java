2ae4f399a2d91e9bc0053a0ebd019b9a80af75f5
olliechick <och26@uclive.ac.nz>
2017-08-22 12:40:09 +1200

    created basic file reader and testcode

--- src/main/java/seng202/team1/FileProcessor.java
+++ src/main/java/seng202/team1/FileProcessor.java
@@ -1,17 +1,60 @@
 package seng202.team1;
 
+import java.io.*;
+import java.util.ArrayList;
+
 /**
- * Created by och26 on 22/08/17.
+ * Class to handle file processing.
+ * Last updated 22 August 2017.
+ * @author Ollie Chick
  */
 public class FileProcessor {
 
-    public loadCSV(String filename) {
+    public ArrayList<String> loadCSV(String filename) throws FileNotFoundException {
+
+        ArrayList<String> data = new ArrayList<String>();
+        String line;
+        try {
+            Reader inputFile;
+            inputFile = new FileReader(filename);
+
+            BufferedReader bufferReader = new BufferedReader(inputFile);
+
+            while ((line = bufferReader.readLine()) != null) {
+                data.add(line);
+            }
+
+            bufferReader.close(); //tidy up after reading file
+            inputFile.close();
+        } catch (IOException e) {
+            //If there is an IO error here just give up
+            System.err.println("Error while reading file line by line: " + e.getMessage());
+            System.exit(0);
+        }
+
+        return data;
 
     }
 
-    public saveCSV(String filename) {
+    public void saveCSV(String filename) {
 
 
+    }
+
+
+    public static void main( String[] args )
+    {
+        String filename = "bikedata.csv";
+        ArrayList<String> data = new ArrayList<String>();
+        FileProcessor fp = new FileProcessor();
+        try {
+            data = fp.loadCSV(filename);
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("Error");
+        }
+        for (int i = 0; i < data.size(); i++)
+        System.out.println(data.get(i));
     }
 
 }
