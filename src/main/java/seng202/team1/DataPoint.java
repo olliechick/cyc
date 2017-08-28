2bc21e21097d2a3b25c9b4907b1796e40bef45f7
Ollie Chick <och26@uclive.ac.nz>
2017-08-28 17:41:59 +1200

    Add toStrings to Retailer and Wifi, add equals to DataPoint

--- src/main/java/seng202/team1/DataPoint.java
+++ src/main/java/seng202/team1/DataPoint.java
@@ -3,6 +3,18 @@ package seng202.team1;
 /**
  * Holding class for generic arraylist types later. closer to an interface. Probably needs a better name
  * @author Josh Burt
+ * @author Ollie Chick
  */
 public class DataPoint {
+
+    @Override
+    public boolean equals(Object otherObject) {
+        if (this == otherObject) {
+            //two objects are the same (i.e. pointers to the same thing)
+            return true;
+        } else {
+            //check if string repr is the same
+            return this.toString().equals(otherObject.toString());
+        }
+    }
 }
