4bf7c0d467e606b7458623069021b86fd6280273
Joshua Bernasconi <jbe113@uclive.ac.nz>
2017-09-03 19:53:35 +1200

    Started working on filtering the retailer data

--- src/main/java/seng202/team1/Controller/TableController.java
+++ src/main/java/seng202/team1/Controller/TableController.java
@@ -2,19 +2,26 @@ package seng202.team1.Controller;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.concurrent.Task;
 import javafx.concurrent.WorkerStateEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
+import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import seng202.team1.BikeTrip;
 import seng202.team1.DataPoint;
 import seng202.team1.RetailerLocation;
-
 import java.io.File;
 import java.util.ArrayList;
 
@@ -31,6 +38,12 @@ public class TableController {
 
     @FXML
     private ComboBox filterAComboBox;
+
+    @FXML
+    private ComboBox filterBComboBox;
+
+    @FXML
+    private ComboBox filterCComboBox;
 
     @FXML
     private Label nameLabel;
@@ -50,11 +63,12 @@ public class TableController {
     private DummyModel model;
     private Stage stage;
 
+    private FilteredList<DataPoint> filteredData;
+
     public void initialize() {
         /**
          * Run automatically when the fxml is loaded by an FXMLLoader
          */
-        filterAComboBox.getItems().addAll("A", "B", "C", "D");
 
         // Get the selected row on double click and run the data popup
         table.setRowFactory( tv -> {
@@ -71,9 +85,6 @@ public class TableController {
     }
 
     protected void setName() {
-        /**
-         * Can't be included in the init method as the model needs to be init first
-         */
         nameLabel.setText("Logged in as: " + model.getName());
         nameLabel.setVisible(true);
     }
@@ -81,11 +92,39 @@ public class TableController {
     public void filterA() {
         /**
          * Called each time the user chooses an option in the first filter combobox
+         * Uses the filteredList to filter out the rows that don't match the criteria.
+         * More logic can be added for further filtering.
          */
         System.out.println(filterAComboBox.getValue());
+        String filter = (String) filterAComboBox.getValue();
+
+        //Set the filtering criteria, TODO learn lambda in java
+        filteredData.setPredicate(dataPoint -> {
+            RetailerLocation location = (RetailerLocation) dataPoint;
+
+            if (((RetailerLocation) dataPoint).getSecondaryFunction().equals(filter)) {
+                return true;
+            } else {
+                return false;
+            }
+        });
+
+    }
+
+    private void setFilters() {
+        /**
+         * not what it's supposed to filter on, just playing around
+         * testing only at the moment
+         */
+        filterAComboBox.getItems().addAll("Candy & Chocolate", "Newsstands", "Nail Salon");
+
     }
 
     private String getCsvFilename() {
+        /**
+         * Opens a filechooser popup, allowing the user to choose a file.
+         * Only allows for opening of .csv files
+         */
 
         String filename = null;
 
@@ -104,6 +143,11 @@ public class TableController {
     }
 
     private void importBikeCsv(final String filename) {
+        /**
+         * Creates a task to run on another thread to open the file,
+         * to stop GUI hangs.
+         * Also sets the loading animation going and stops when finished.
+         */
 
         final Task<ArrayList<BikeTrip>>loadBikeCsv = new Task<ArrayList<BikeTrip>>() {
             /**
@@ -124,9 +168,8 @@ public class TableController {
 
         loadBikeCsv.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
             /**
-             * Fairly certain this won't get called until the runLater in the task completes.
-             * So should be safe spot to stop the loading animations.
-             *
+             * Runs only once the task has finished, gives the gui the loaded
+             * data and updates it.
              */
             public void handle(WorkerStateEvent event) {
 
@@ -139,6 +182,10 @@ public class TableController {
     }
 
     private void importRetailerCsv(final String filename) {
+        /**
+         * Same as importBikeCsv but is needed as it was the only way to get the task
+         * to call the correct CSVLoader methods.
+         */
 
         final Task<ArrayList<RetailerLocation>> loadRetailerCsv = new Task<ArrayList<RetailerLocation>>() {
             /**
@@ -202,7 +249,6 @@ public class TableController {
         }
     }
 
-
     private void setTableViewRetailer(ArrayList<RetailerLocation> data) {
         /**
          * Pretty much straight from http://docs.oracle.com/javafx/2/ui_controls/table-view.htm
@@ -210,23 +256,46 @@ public class TableController {
 
         ObservableList<DataPoint> dataPoints = FXCollections.observableArrayList(data);
 
-
+        // Create the columns
         TableColumn nameCol = new TableColumn("Name");
         TableColumn addressCol = new TableColumn("Address");
         TableColumn primaryCol = new TableColumn("Primary Function");
         TableColumn secondaryCol = new TableColumn("Secondary Function");
+
+        //Set the IDs of the columns, not used yet TODO remove if never use
+        nameCol.setId("name");
+        addressCol.setId("address");
+        primaryCol.setId("primary");
+        secondaryCol.setId("secondary");
+
+        //Clear the default columns, or any columns in the table.
         table.getColumns().clear();
 
+        //Sets up each column to get the correct entry in each dataPoint
         nameCol.setCellValueFactory( new PropertyValueFactory<DataPoint, String>("name"));
         addressCol.setCellValueFactory( new PropertyValueFactory<DataPoint, String>("address"));
         primaryCol.setCellValueFactory( new PropertyValueFactory<DataPoint, String>("primaryFunction"));
         secondaryCol.setCellValueFactory( new PropertyValueFactory<DataPoint, String>("secondaryFunction"));
 
-        table.setItems(dataPoints);
+        // Next few lines allow for easy filtering of the data using a FilteredList and SortedList
+        filteredData = new FilteredList<DataPoint>(dataPoints, p -> true);
+
+        SortedList<DataPoint> sortedData = new SortedList<DataPoint>(filteredData);
+        sortedData.comparatorProperty().bind(table.comparatorProperty());
+
+        // Add the sorted and filtered data to the table.
+        table.setItems(sortedData);
         table.getColumns().addAll(nameCol, addressCol, primaryCol, secondaryCol);
+
+        // Initialise the values in the filter comboboxes now that we have data to work with
+        setFilters();
     }
 
     private void setTableViewBike(ArrayList<BikeTrip> data) {
+        /**
+         * Fairly similar to Retailer setup, but for a bike trip
+         * TODO finish
+         */
 
         ObservableList<DataPoint> dataPoints = FXCollections.observableArrayList(data);
         TableColumn durationCol = new TableColumn("Duration");
@@ -259,6 +328,10 @@ public class TableController {
     }
 
     protected void initModel(DummyModel dummyModel) {
+        /**
+         * initialises the model for use in the rest of the View
+         * Will allow for accessing user data once implemented
+         */
         this.model = dummyModel;
     }
 }
