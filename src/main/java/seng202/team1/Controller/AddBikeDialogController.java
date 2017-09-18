946f34a9fe9c5a638235869fcba683cddbaca3db
Joshua Bernasconi <jbe113@uclive.ac.nz>
2017-09-19 11:22:25 +1200

    Changes for new bike trip constructor

--- src/main/java/seng202/team1/Controller/AddBikeDialogController.java
+++ src/main/java/seng202/team1/Controller/AddBikeDialogController.java
@@ -153,7 +153,7 @@ public class AddBikeDialogController {
 //                    new Point.Float(1, 1), new Point.Float(2, 2), 123, 'm', 1997);
 //            //System.out.println(bikeTrip);
 //            stage.close();
-            bikeTrip = new BikeTrip(startDateTime, stopDateTime, startPoint, endPoint, bikeID, gender, birthYear);
+            bikeTrip = new BikeTrip(startDateTime, stopDateTime, startPoint, endPoint, bikeID, gender, birthYear, false);
             stage.close();
         }
     }
