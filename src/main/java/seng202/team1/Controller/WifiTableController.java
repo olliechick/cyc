14ab4277d9967db4bfef48ad083008c30ebb9795
Joshua Bernasconi <jbe113@uclive.ac.nz>
2017-09-21 10:20:55 +1200

    Merge ManuallyEnterWifi

--- src/main/java/seng202/team1/Controller/WifiTableController.java
+++ src/main/java/seng202/team1/Controller/WifiTableController.java
@@ -9,6 +9,8 @@ import javafx.concurrent.Task;
 import javafx.concurrent.WorkerStateEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
