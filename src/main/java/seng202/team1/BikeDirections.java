c4f6c7eb22ac3543e7ad9a7fb2824b38d6ed54bb
Ollie Chick <och26@uclive.ac.nz>
2017-09-21 12:09:29 +1200

    Create BikeDirections class

---
+++ src/main/java/seng202/team1/BikeDirections.java
@@ -0,0 +1,12 @@
+package seng202.team1;
+
+import java.awt.Point;
+
+public class BikeDirections {
+
+    private String overviewPolyline;
+    private Point.Float overviewStartPoint;
+    private Point.Float overviewEndPoint;
+
+
+}
