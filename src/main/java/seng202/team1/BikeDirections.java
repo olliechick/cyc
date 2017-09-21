108adf20fb03920c99cd31eb69a14bd4e7e427a2
Ollie Chick <och26@uclive.ac.nz>
2017-09-21 12:23:14 +1200

    Add localdate dateRetrieved to BikeDirections

--- src/main/java/seng202/team1/BikeDirections.java
+++ src/main/java/seng202/team1/BikeDirections.java
@@ -1,6 +1,7 @@
 package seng202.team1;
 
 import java.awt.Point;
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class BikeDirections {
@@ -8,6 +9,6 @@ public class BikeDirections {
     private String overviewPolyline;
     private Point.Float overviewStartPoint;
     private Point.Float overviewEndPoint;
-    private ArrayList<String>
+    private LocalDate dateRetrieved;
 
 }
