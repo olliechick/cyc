a80ef3c4c737cec64b23e674d8336fdfa9fdddb7
Ollie Chick <och26@uclive.ac.nz>
2017-09-21 12:19:18 +1200

    .

--- src/main/java/seng202/team1/BikeDirections.java
+++ src/main/java/seng202/team1/BikeDirections.java
@@ -1,12 +1,13 @@
 package seng202.team1;
 
 import java.awt.Point;
+import java.util.ArrayList;
 
 public class BikeDirections {
 
     private String overviewPolyline;
     private Point.Float overviewStartPoint;
     private Point.Float overviewEndPoint;
-
+    private ArrayList<String>
 
 }
