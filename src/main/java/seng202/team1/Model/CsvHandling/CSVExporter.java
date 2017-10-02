989edc306100618473bf09d40afc1fb689e97bb3
Ollie Chick <och26@uclive.ac.nz>
2017-10-02 23:56:40 +1300

    Finish exportBikeTrips (currently just gets the bike trips from the packaged CSV, not the database

--- src/main/java/seng202/team1/Model/CsvHandling/CSVExporter.java
+++ src/main/java/seng202/team1/Model/CsvHandling/CSVExporter.java
@@ -7,16 +7,22 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
  * Exports a list of DataPoints to a CSV file.
+ * These CSV files are able to be (edited and) re-imported into the app.
+ * Note that some columns will be blank - these are
  *
  * @author Ollie Chick
  */
 public class CSVExporter {
+
+    private final static String bikeTripsHeader = "Trip duration,Start time,Stop time,,,Start point latitude,"
+            + "Start point longitude,,,End point latitude,End point longitude,Bike ID,,Birth year,Gender";
+    public final static String datetimeFormat = "M/d/yyyy HH:mm:ss";
 
     /**
      * Exports a list of Strings as lines into the file (whose name is given by filename).
@@ -32,20 +38,55 @@ public class CSVExporter {
 
 
     /**
-     * Gets the bike trips from the database and saves them to the file whose name is given by filename.
+     * Gets the bike trips from the database associated with the user denoted by username
+     * and saves them to the file whose name is given by filename.
      *
      * @param filename The name of the file to save
+     * @param username Username of the user whose bike trips will be exported
+     * @throws IOException If an IO Exception occurs
      */
-    public static void exportBikeTrips(String filename) throws IOException{
-        //TODO get bike trips from database
-        List<String> lines = Arrays.asList("BT1", "BT2");
+    public static void exportBikeTrips(String filename, String username) throws IOException {
+        ArrayList<BikeTrip> bikeTrips = new ArrayList<>();
+        //bikeTrips = getUserTrips(username);
+        try {
+            bikeTrips = CSVLoader.populateBikeTrips();
+        } catch (CsvParserException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        ArrayList<String> lines = new ArrayList<String>();
+        lines.add(bikeTripsHeader);
+        for (BikeTrip bikeTrip : bikeTrips) {
+            String line;
+            line = bikeTrip.getTripDuration() + ",";
+            line += bikeTrip.getStartTime().format(DateTimeFormatter.ofPattern(datetimeFormat)) + ",";
+            line += bikeTrip.getStopTime().format(DateTimeFormatter.ofPattern(datetimeFormat)) + ",";
+            line += ",,"; //skip start station id and name
+            line += bikeTrip.getStartLatitude() + ",";
+            line += bikeTrip.getStartLongitude() + ",";
+            line += ",,"; //skip end station id and name
+            line += bikeTrip.getEndLatitude() + ",";
+            line += bikeTrip.getEndLongitude() + ",";
+            line += bikeTrip.getBikeId() + ",";
+            line += ","; //skip usertype (Customer/Subscriber)
+            line += bikeTrip.getBirthYear() + ",";
+            char gender = bikeTrip.getGender();
+            if (gender == 'm') {
+                line += "1,";
+            } else if (gender == 'f') {
+                line += "2,";
+            } else {
+                line += "0,";
+            }
+            lines.add(line);
+        }
         exportCSV(filename, lines);
     }
 
 
     public static void main(String[] args) {
         try {
-            exportBikeTrips("myBikeTrips.csv");
+            exportBikeTrips("myBikeTrips.csv", "a");
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
