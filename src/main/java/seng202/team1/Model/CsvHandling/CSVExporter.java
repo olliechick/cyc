10c3593b171b7661cfba12f0fbe348bcb0a102c4
Ollie Chick <och26@uclive.ac.nz>
2017-10-02 13:59:56 +1300

    Start CSVExporter.exportBikeTrips

--- src/main/java/seng202/team1/Model/CsvHandling/CSVExporter.java
+++ src/main/java/seng202/team1/Model/CsvHandling/CSVExporter.java
@@ -1,10 +1,13 @@
 package seng202.team1.Model.CsvHandling;
 
+import seng202.team1.Model.BikeTrip;
+
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -16,10 +19,10 @@ import java.util.List;
 public class CSVExporter {
 
     /**
-     * Exports a list of Strings as lines into the file (whose name is given by filename)
+     * Exports a list of Strings as lines into the file (whose name is given by filename).
      *
      * @param filename The name of the file to save
-     * @param lines A list of lines to save to the file
+     * @param lines    A list of lines to save to the file
      * @throws IOException If an IO Exception occurs
      */
     public static void exportCSV(String filename, List<String> lines) throws IOException {
@@ -28,10 +31,21 @@ public class CSVExporter {
     }
 
 
+    /**
+     * Gets the bike trips from the database and saves them to the file whose name is given by filename.
+     *
+     * @param filename The name of the file to save
+     */
+    public static void exportBikeTrips(String filename) throws IOException{
+        //TODO get bike trips from database
+        List<String> lines = Arrays.asList("BT1", "BT2");
+        exportCSV(filename, lines);
+    }
+
+
     public static void main(String[] args) {
         try {
-            List<String> lines = Arrays.asList("The first line", "The second line");
-            exportCSV("test", lines);
+            exportBikeTrips("myBikeTrips.csv");
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
