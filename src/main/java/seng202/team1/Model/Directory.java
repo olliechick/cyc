47369869d27f996bf3ebfec1b94776ac221343e0
Ollie Chick <och26@uclive.ac.nz>
2017-10-06 22:58:23 +1300

    javadoc Directory enum

--- src/main/java/seng202/team1/Model/Directory.java
+++ src/main/java/seng202/team1/Model/Directory.java
@@ -1,5 +1,12 @@
 package seng202.team1.Model;
 
+/**
+ * Directory enum that stores the names of all the directories necessary for the app.
+ * These are used to create them if they don't exist and so the app knows where to
+ * get files from.
+ *
+ * @author Ollie Chick
+ */
 public enum Directory {
 
     ROOT(System.getProperty("user.home") + "/.cyc/"),
